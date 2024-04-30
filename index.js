5d1fcd30c1514cd51ad2cf19fab1a51266ab4dfc
jerrintm <140678144+jerrintm@users.noreply.github.com>
2024-04-29 22:21:09 -0400

    Changes for Task 2.9

--- index.js
+++ index.js
@@ -154,7 +154,8 @@ let users = [
 //    res.json(movies);
 //});
 
-app.get('/movies', async (req, res) => {
+/* app.get('/movies', async (req, res) => { */
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
